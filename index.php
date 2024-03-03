8f281f76f42c5e07affbb76f1f03dcc0dc6c4595
agostinoluca <agostino.luca89@gmail.com>
2024-03-03 12:20:15 +0100

    add form

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+Gestire il tutto con due file diversi. Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+- stampare a schermo il paragrafo e la sua lunghezza
+- stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badworks</title>
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+    <div class="container-sm m-auto p-4 ">
+        <form action="" method="post" class="d-flex flex-column align-items-center gap-2">
+            <textarea placeholder="Enter your paragraph here" name="paragraph" id="paragraph" cols="50" rows="3"></textarea>
+            <input type="text" name="censure" id="censure" class="w-50">
+            <input type="submit" value="Send">
+        </form>
+    </div>
+</body>
+
+</html>
+
+<!doctype html>
+<html lang="en">
